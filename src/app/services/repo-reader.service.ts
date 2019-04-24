9984cf3101fcbf337f4f1579b59dc28ccd0a0822
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-04-24 19:30:38 +0200

    added basic service implementation

--- src/app/services/repo-reader.service.ts
+++ src/app/services/repo-reader.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoReaderService {
+  private apiUrl: string = environment.apiUrl;
+  offset = new Date().getTimezoneOffset();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getRepositories(user): any {
+
+    let req_headers = new HttpHeaders();
+    req_headers.set('userTimeZone', this.offset.toString());
+
+    this.http.get(`${this.apiUrl}/api/v1/getRepos/${user}`,
+      { headers: req_headers }
+    );
+
+  };
+
 }
